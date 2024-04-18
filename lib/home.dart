01c3e2730dae557c22c7a10466fb896b26114def
MuhammadIbnuA <muhammad2100016028@webmail.uad.ac.id>
2024-04-18 15:18:56 +0700

    run prak4

--- lib/home.dart
+++ lib/home.dart
@@ -27,7 +27,7 @@ class HomePage extends StatelessWidget {
       // TODO: Add app bar (102)
       // TODO: Add a grid view (102)
       body: Center(
-        child: Text('You did it!'),
+        child: Text('You did it! anjay'),
       ),
       resizeToAvoidBottomInset: false,
       // TODO: Set resizeToAvoidBottomInset (101)
